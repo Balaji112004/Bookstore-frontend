fad0372f3ed517a51a8833fcff009660adeee51f
Balaji112004 <k.balaji112004@gmail.com>
2025-11-16 19:48:50 +0530

    Remove cartSubmit function and button from BookDesign
    Removed cartSubmit function and related button from BookDesign component.

--- src/BookDesign.jsx
+++ src/BookDesign.jsx
@@ -1,28 +1,8 @@
 import React, { useContext } from "react";
-import axios from "axios"; // you missed importing axios
 import { UserContext } from "./context/UserContext";
 
 function BookDesign({ trend }) {
   const { user } = useContext(UserContext);
-
-  const cartSubmit = async () => {
-    if (!user) {
-      alert("Please login first!");
-      return;
-    }
-
-    try {
-      const res = await axios.post(
-        `https://bookstorebackend-production-f262.up.railway.app/api/cartAdd/${trend.id}`,
-        { userId: user.id }
-      );
-      console.log(res.data);
-      alert("Added to cart!");
-    } catch (err) {
-      console.error("Error adding to cart:", err);
-      alert("Failed to add to cart!");
-    }
-  };
 
   return (
     <div className="lg:w-[210px] w-[150px] h-[360px] lg:h-[420px] gap-6 bg-white rounded-xl shadow p-2 lg:p-4 flex-row items-center justify-center">
@@ -37,7 +17,9 @@ function BookDesign({ trend }) {
       </div>
       <div>
         <center>
-          <span className="font-semibold text-md lg:text-xl mb-2 block mt-4">{trend.title}</span>
+          <span className="font-semibold text-md lg:text-xl mb-2 block mt-4">
+            {trend.title}
+          </span>
         </center>
       </div>
       <div className="mt-4 lg:mt-2">
@@ -50,16 +32,9 @@ function BookDesign({ trend }) {
           </del>
         </center>
       </div>
-      <center>
-        <button
-          onClick={cartSubmit}
-          className="bg-blue-500 text-white px-4 py-2 rounded mt-2"
-        >
-          Add to Cart
-        </button>
-      </center>
     </div>
   );
 }
 
 export default BookDesign;
+
